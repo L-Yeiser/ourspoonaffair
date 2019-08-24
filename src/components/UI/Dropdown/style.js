bbbd2c1c2a7f102db762683bf92e43f70b23f6e9
L-Yeiser <lauren.a.yeiser@gmail.com>
2019-08-24 07:03:52 -0700

    update casing of UI

---
+++ src/components/UI/Dropdown/style.js
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+
+import { standards, colors } from 'themes';
+
+export const Container = styled.div`
+  background-color: ${colors.grayDarkest};
+  border-radius: 2px;
+  box-shadow: 0px 8px 16px 0px ${colors.grayLightest};
+  display: flex;
+  min-width: 175px;
+  opacity: ${({ isOpen }) => (isOpen ? 1 : 0)};
+  padding: ${standards.padding};
+  position: absolute;
+  right: 0px;
+  top: 30px;
+  transition: visibility 0s, opacity 0.3s ease-in-out;
+
+  .content {
+    flex: 1;
+  }
+
+  .link {
+    display: flex;
+    justify-content: flex-end;
+    flex: 1;
+  }
+
+  a {
+    color: ${colors.white};
+
+    &.active {
+      color: ${colors.grayLight};
+    }
+  }
+`;
---
+++ src/components/UI/MenuButton/style.js
@@ -0,0 +1,98 @@
+import styled from '@emotion/styled';
+
+import { colors } from 'themes';
+
+export const Button = styled.div`
+  width: 30px;
+  height: 25px;
+  position: relative;
+  border: none;
+  overflow: hidden;
+
+  span {
+    position: absolute;
+    height: 20%;
+    width: 50%;
+    background: ${colors.grayDark};
+    opacity: 1;
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+    -webkit-transition: 0.25s ease-in-out;
+    -moz-transition: 0.25s ease-in-out;
+    -o-transition: 0.25s ease-in-out;
+    transition: 0.25s ease-in-out;
+  }
+
+  span:nth-child(even) {
+    left: 50%;
+    border-radius: 0 6px 6px 0;
+  }
+
+  span:nth-child(odd) {
+    left: 0px;
+    border-radius: 6px 0 0 6px;
+  }
+
+  span:nth-child(1),
+  span:nth-child(2) {
+    top: 0px;
+  }
+
+  span:nth-child(3),
+  span:nth-child(4) {
+    top: 40%;
+  }
+
+  span:nth-child(5),
+  span:nth-child(6) {
+    top: 80%;
+  }
+
+  .open span:nth-child(1),
+  .open span:nth-child(6) {
+    -webkit-transform: rotate(45deg);
+    -moz-transform: rotate(45deg);
+    -o-transform: rotate(45deg);
+    transform: rotate(45deg);
+  }
+
+  .open span:nth-child(2),
+  .open span:nth-child(5) {
+    -webkit-transform: rotate(-45deg);
+    -moz-transform: rotate(-45deg);
+    -o-transform: rotate(-45deg);
+    transform: rotate(-45deg);
+  }
+
+  .open span:nth-child(1) {
+    left: calc(10%);
+    top: calc(15%);
+  }
+
+  .open span:nth-child(2) {
+    left: calc(40%);
+    top: calc(15%);
+  }
+
+  .open span:nth-child(3) {
+    left: -50%;
+    opacity: 0;
+  }
+
+  .open span:nth-child(4) {
+    left: 100%;
+    opacity: 0;
+  }
+
+  .open span:nth-child(5) {
+    left: calc(10%);
+    top: calc(58%);
+  }
+
+  .open span:nth-child(6) {
+    left: calc(40%);
+    top: calc(58%);
+  }
+`;
