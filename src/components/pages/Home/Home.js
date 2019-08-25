de7789464e46486c8bbb3d86c8f13b54bf8af15d
L-Yeiser <lauren.a.yeiser@gmail.com>
2019-08-24 19:36:25 -0700

    add text for landing page

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-const HomeWrapper = styled.div``;
+import { Container } from './style';
 
 export const Home = () => {
-  return <HomeWrapper />;
+  return (
+    <Container>
+      <h1>
+        <span>
+          Kate Willson <span className="hideLarge">And</span>
+          <span className="hideSmall"> +</span>
+        </span>{' '}
+        <span>Lauren Yeiser</span>
+      </h1>
+      <p>
+        <span>Saturday, Sept 5, 2020</span>{' '}
+        <span className="hideSmall">| </span>
+        <span>Mt Hood, Oregon</span>
+      </p>
+    </Container>
+  );
 };
--- src/components/pages/Home/style.js
+++ src/components/pages/Home/style.js
@@ -1,4 +1,36 @@
 /** @jsx jsx */
 import styled from '@emotion/styled';
 
-import { constants } from 'themes';
+import { standards, constants } from 'themes';
+
+export const Container = styled.div`
+  margin-top: ${standards.marginLarge};
+  padding: ${standards.padding};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+
+  h1,
+  p {
+    text-align: center;
+
+    span.hideLarge {
+      display: none;
+    }
+
+    ${[constants.mediaQuery('small')]} {
+      span {
+        display: block;
+      }
+      span.hideSmall {
+        display: none;
+      }
+
+      span.hideLarge {
+        display: inherit;
+      }
+    }
+  }
+`;
